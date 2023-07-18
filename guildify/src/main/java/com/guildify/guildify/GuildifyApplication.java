7465a954ec18c56c6e3f91004158ce2bcd2a6b43
ecvsgl <efecav95@gmail.com>
2023-07-18 21:27:22 +0300

    first commit

---
+++ guildify/src/main/java/com/guildify/guildify/GuildifyApplication.java
@@ -0,0 +1,13 @@
+package com.guildify.guildify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuildifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuildifyApplication.class, args);
+    }
+
+}
