94e79aca10ac83646bd3159dd155eb5e5f41bffa
Efe Çavuşoğlu <efecav95@gmail.com>
2023-07-24 16:25:09 +0300

    Foldering structure changed due GIT errors.

---
+++ src/test/java/com/guildify/guildify/GuildifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guildify.guildify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuildifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
