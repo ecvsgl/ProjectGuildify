31e0021c9c556adeb9daa192fab7d4aaf87fd004
Efe Çavuşoğlu <efecav95@gmail.com>
2023-07-26 17:46:05 +0300

    User entity, repo, service WIP

---
+++ src/main/java/com/guildify/guildify/service/UserService.java
@@ -0,0 +1,46 @@
+package com.guildify.guildify.service;
+
+import com.guildify.guildify.model.UserEntity;
+import com.guildify.guildify.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.security.MessageDigest;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+
+
+    public UserEntity getSpecificUserEntity(Integer userId){
+
+        return userRepository.findUserByUserId(userId);
+    }
+
+    public void deleteUserEntity(Integer userId){
+        userRepository.delete(userRepository.findUserByUserId(userId));
+    }
+
+    public void updateUserEntityPassword(Integer userId, String newPassword){
+        UserEntity userEntity = userRepository.findUserByUserId(userId);
+        userEntity.setPasswordHash(stringHashingMethod(newPassword));
+    }
+
+    public static String stringHashingMethod(String inputString){
+        StringBuilder sb = new StringBuilder();
+        try{
+            MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+            byte[] hashes = messageDigest.digest(inputString.getBytes());
+            for (byte a: hashes){
+                sb.append(String.format("%02X",a));
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return sb.toString();
+    }
+}
