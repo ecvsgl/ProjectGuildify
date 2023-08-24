57a83c717b31e0abbb9a68e466a3354d5655518e
Efe Çavuşoğlu <efecav95@gmail.com>
2023-08-24 13:20:11 +0300

    GameRepo query by gamename added.

--- src/main/java/com/guildify/guildify/repository/GameRepository.java
+++ src/main/java/com/guildify/guildify/repository/GameRepository.java
@@ -8,4 +8,6 @@ import org.springframework.stereotype.Repository;
 public interface GameRepository extends JpaRepository<GameEntity, Integer> {
 
     GameEntity findGameEntityByGameId(int gameId);
+
+    GameEntity findGameEntityByGameName(String s);
 }
